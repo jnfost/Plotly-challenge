1259e49a0cca9e40ac5283b7a49b3ade9847423f
Jewell Foster <60074350+thinice209@users.noreply.github.com>
2020-06-23 19:53:27 -0400

    Bar chart is better
    Need to add OTU Ids

--- app.js
+++ app.js
@@ -15,21 +15,22 @@ function buildCharts(sample) {
         zippedSample = d3.zip(actualSample.otu_ids, actualSample.otu_labels, actualSample.sample_values);
         // console.log(zippedSample);
         
-        // Sort sample values ascending
-        sortedSample = zippedSample.sort((a,b) => a[2] - b[2]);
+        // Sort sample values descending
+        sortedSample = zippedSample.sort((a,b) => b[2] - a[2]);
         // console.log(sortedSample);
 
         // // Slice to get top 10 sample values (last 10 entries)
-        topSample = sortedSample.slice(-10);
+        topSample = sortedSample.slice(0,10);
         console.log(topSample);
         reversedSample = topSample.reverse();
         console.log(reversedSample);
        
-    
+        otuIds = reversedSample.map(testSample => testSample[0].toString());
+        console.log(otuIds);
         // Create bar chart (values = sample_values[2], labels= otu_ids[0], hovertext = otu_labels[1])
         var trace1 = {
             x: reversedSample.map(testSample => testSample[2]),
-            y: reversedSample.map(testSample => testSample[0].toString()),
+            // y: reversedSample.map(testSample => testSample[0].toString()),
             hovertext: reversedSample.map(testSample => testSample[1]),
             type: "bar",
             orientation: "h"
@@ -39,6 +40,13 @@ function buildCharts(sample) {
 
         var layout= {
             title: `Top 10 Bacteria for Sample # ${sample}`,
+            xaxis: {
+                title: "Sample Values"
+            },
+            yaxis: {
+                text: `OTU ID ${otuIds}`
+            },
+            
             
             // margin: {
             //     l: 100,
@@ -74,51 +82,51 @@ function buildCharts(sample) {
     })
 }
 
-function buildGauge(sample){
-    d3.json("/data/samples.json").then(function(data) {
-        console.log(data);
+// function buildGauge(sample){
+//     d3.json("/data/samples.json").then(function(data) {
+//         console.log(data);
 
-        // Find data for selected sample
-        var sampleData = data.metadata;
-        console.log(sampleData);
-        console.log(sample);
-        var filteredSample = sampleData.filter(function(sampleObj){
-            return sampleObj.id == sample;
-        })
-        actualSample = filteredSample[0];
-        console.log(actualSample);
+//         // Find data for selected sample
+//         var sampleData = data.metadata;
+//         console.log(sampleData);
+//         console.log(sample);
+//         var filteredSample = sampleData.filter(function(sampleObj){
+//             return sampleObj.id == sample;
+//         })
+//         actualSample = filteredSample[0];
+//         console.log(actualSample);
 
-        // Build gauge plot
-        var data = [
-            {
-              type: "indicator",
-              value: 200,
-              delta: { reference: 160 },
-              gauge: { axis: { visible: false, range: [0, 250] } },
-              domain: { row: 0, column: 0 }
-            }];
-        // var trace3 = [
-        //     {
-        //         domain: { x: [0, 1], y: [0, 1] },
-        //         value: actualSample.wfreq,
-        //         title: { text: "Belly Button Washing Frequency (Scrubs per Week)" },
-        //         type: "indicator",
-        //         mode: "gauge+number",
-        //         gauge: { 
-        //             axis: {
-        //                 visible: false,
-        //                 range: [0-7]
-        //             },
-        //         },
+//         // Build gauge plot
+//         var data = [
+//             {
+//               type: "indicator",
+//               value: 200,
+//               delta: { reference: 160 },
+//               gauge: { axis: { visible: false, range: [0, 250] } },
+//               domain: { row: 0, column: 0 }
+//             }];
+//         // var trace3 = [
+//         //     {
+//         //         domain: { x: [0, 1], y: [0, 1] },
+//         //         value: actualSample.wfreq,
+//         //         title: { text: "Belly Button Washing Frequency (Scrubs per Week)" },
+//         //         type: "indicator",
+//         //         mode: "gauge+number",
+//         //         gauge: { 
+//         //             axis: {
+//         //                 visible: false,
+//         //                 range: [0-7]
+//         //             },
+//         //         },
 
-        //     }
-        // ];
-        // var data2 = [trace3];
-        var layout = { width: 500, height: 400, margin: { t: 10, b: 10 } };
-        Plotly.newPlot("gauge", data, layout);
-    })
+//         //     }
+//         // ];
+//         // var data2 = [trace3];
+//         var layout = { width: 500, height: 400, margin: { t: 10, b: 10 } };
+//         Plotly.newPlot("gauge", data, layout);
+//     })
 
-}
+// }
 
 
 
